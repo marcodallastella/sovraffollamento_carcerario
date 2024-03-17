a8b13de6bf74bca1464a06eca0f6e9a32daf659a
marcodallastella <118016116+marcodallastella@users.noreply.github.com>
2024-03-17 13:50:23 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,56 @@
+# Schiacciati
+
+Author: Marco Dalla Stella
+
+## Intro
+
+This repository contains all the data and notebooks for the project "Schiacciati" (literally: squeezed, but also crushed).\
+\
+The project aims to report on the problem of prison overcrowding in Italy, collecting government data about Italian prisons' capacity and actual detainees over the years. In particular, it was developed in support of the great work being done by the [Associazione Antigone](https://www.antigone.it/), the main Italian organization defending the rights of people deprived of their liberty.\
+\
+I used this project also to practice different scraping techniques, as well as some data visualization. [Here](https://marcodallastella.github.io/schiacciati/) you can find the final website (it's in Italian, but there's not so much text).
+
+## Notebooks
+
+### 1_scraper_bulletins.ipynb
+
+This notebook contains the code I used to scrape the data about monthly bulletins. After some attempts with requests and playwright, I eventually settled on Selenium as it was giving me the best results in having the pages successfully loaded (especially in headless mode).\
+\
+The Ministry of Justice publishes monthly [bulletins](https://www.giustizia.it/giustizia/it/mg_1_14_1.page?contentId=SST459023) about the state of detention centers' inmates, but it does not keep a unique structured database. To get the data, this notebook utilizes three phases:
+
+1) A scraper for the [statistics section](https://www.giustizia.it/giustizia/page/it/statistiche) of the Ministry to identify as many links as possible to the pages containing the data I needed, identified using the string `detenuti italiani e stranieri presenti e capienze per istituto`.
+2) A scraper that visits all the fetched links and stores the HTML code locally. This was convenient as the tables are a bit funky (multi-level headers😠) and some trial-and-error was needed.
+3) Parsing the HTML files with BeautifulSoup to get the data in a structured and semi-clean format. The output is `outputs/raw/bollettini_mensili_raw.csv`.
+
+### 1_scraper_institutes.ipynb
+
+This is a different scraper used to collect data about specific pages of the various detention centers. In fact, in addition to the monthly bulletin, each center publishes on a dedicated page interesting data such as staff presences and room availability. It turns out that the official capacity of many institutes does not reflect the *real* capacity, but this information is not reflected in the monthly bulletins.\
+\
+This notebook then uses Selenium to visit the webpage [https://www.giustizia.it/giustizia/page/it/istituti_penitenziari](https://www.giustizia.it/giustizia/page/it/istituti_penitenziari) and download the source code locally. Because information about specific institutes (including geographic coordinates) is not part of the HTML code but is instead loaded via JavaScript, I made extensive use of RegEx to identify the various pieces of information and put them together.\
+\
+As a result, this part of the code produces `outputs/clean/istituti_penitenziari.csv`, a file containing generic information about all detention centers in Italy (including name, type, address, phone number, geographic coordinates).
+
+
+### 2_scraper_bulletins_monthly
+
+This is a version of the monthly bulletins scraper designed to run once a month to fetch data about recently published bulletins and append the data to the existing `bollettini_mensili_raw` CSV file.
+
+### 2_scraper_institutes_monthly
+
+This notebook uses the previously fetched ID numbers of the various detention centers to determine the single webpages for every institute, as they are all in the format `https://www.giustizia.it/giustizia/it/dettaglio_scheda.page?s={id_number}`.\
+\
+It uses Selenium to store the source code of the various institutes' webpages locally and then uses BeautifulSoup to parse it and extract relevant information. The output is `outputs/clean/istituti_penitenziari_data.csv`.
+
+### 3_cleaning_bulletins.ipynb
+
+I used this notebook for all the basic cleaning and formatting numbers correctly. However, the detention centers appear in the various bulletins with dozens of different spellings that needed special attention.\
+\
+To clean and standardize the detention centers' names, I used the previously fetched institutes' names and NLTK to tokenize the various names, find the best match, and use it to replace the previous names. Because this method is not 100% proof, some manual checks were needed. The output is stored in `outputs/clean/istituti_penitenziari.csv`.
+
+
+### 3-analysis.ipynb
+I used this notebook to analyze the data and extract tables that I thought could be useful to create visualizations.\
+\
+For any questions, inquiries, critiques, or suggestions, please do not hesitate to reach me at [md3934@columbia.edu](mailto:md3934@columbia.edu).\
+\
+Marco Dalla Stella
---
+++ docs/src/totals.js
@@ -0,0 +1,232 @@
+let margin = {
+  top: 20,
+  right: 100,
+  bottom: 30,
+  left: 70
+};
+
+
+const chartContainer = d3.select('div#chart');
+const currentWidth = parseInt(chartContainer.style('width'), 10);
+let width = currentWidth - margin.left - margin.right;
+const height = 300 - margin.top - margin.bottom;
+
+let svg = chartContainer
+  .append('svg')
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+  .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+let scaleX = d3.scaleTime()
+  .range([0, width]);
+
+let scaleY = d3.scaleLinear()
+  .domain([45000, 65000])
+  .range([height, 0]);
+// Define the y-axis
+let yAxis = d3.axisLeft(scaleY).ticks(5, ',r').tickSize(-width);
+
+// Append the y-axis
+let yAxisGroup = svg.append("g")
+  .attr("class", "y-axis")
+  .call(yAxis);
+
+// Define the x-axis
+let xAxis = d3.axisBottom(scaleX).tickSize(-height);
+
+// Append the x-axis
+let xAxisGroup = svg.append("g")
+  .attr("class", "x-axis")
+  .attr("transform", "translate(0," + height + ")")
+  .call(xAxis)
+
+
+let line;
+let line2;
+let data;
+let lastDataPoint;
+
+
+
+// Read the data
+d3.csv("https://raw.githubusercontent.com/marcodallastella/schiacciati/main/outputs/clean/occupancy_rate_total.csv",
+  // When reading the csv, I must format variables:
+  function (d) {
+    return { date: d3.timeParse("%Y-%m-%d")(d.date), value: d.inmates_total, capacity: d.capacity, rate: d.rate }
+  },
+  // Now I can use this dataset:
+  function (dataSet) {
+    data = dataSet; // Set data to the dataSet
+    // Update the last data point
+    lastDataPoint = data[data.length - 1];
+    scaleX = d3.scaleTime()
+      .domain(d3.extent(data, function (d) { return d.date; }))
+      .range([0, width]);
+
+    // Update the x-axis scale
+    xAxis.scale(scaleX);
+
+    // Redraw the x-axis with the new scale
+    xAxisGroup.call(xAxis);
+
+    // svg.append("g")
+    //   .attr("class", "x-axis")
+    //   .attr("transform", "translate(0," + height + ")")
+    //   .call(d3.axisBottom(scaleX));
+
+    // Define the line function with curve interpolation
+    line = d3.line()
+      .x(function (d) { return scaleX(d.date); })
+      .y(function (d) { return scaleY(d.value); })
+      .curve(d3.curveLinear);
+
+    // Add the total line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", '#EC1330')
+      .attr("stroke-width", 1.5)
+      .attr("d", line)
+      .attr("class", "line-path");
+
+    // Define the line function with curve interpolation
+    line2 = d3.line()
+      .x(function (d) { return scaleX(d.date); })
+      .y(function (d) { return scaleY(d.capacity); })
+      .curve(d3.curveLinear);
+
+    // Add the capacity line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", 'blue')
+      .attr("stroke-width", 1.5)
+      .attr("d", line2)
+      .attr("class", "line-path2");
+
+    // Add a circle at the end of the line
+    svg.append("circle")
+      .attr("id", "valueCircle")
+      .attr("cx", scaleX(lastDataPoint.date))
+      .attr("cy", scaleY(lastDataPoint.value))
+      .attr("r", 5) // radius
+      .attr("fill", "white")
+      .attr("stroke", "#EC1330")
+      .attr("stroke-width", 1.5);
+
+    // Add a text element for the year
+    svg.append("text")
+      .attr("id", "yearText")
+      .attr("x", scaleX(lastDataPoint.date))
+      .attr("y", scaleY(lastDataPoint.value) - 25) // position the text slightly above the circle
+      .text(`${lastDataPoint.date.getFullYear()}`)
+      .attr("font-size", "1.7vh")
+      .attr("text-anchor", "left")
+      .style("font-weight", "bold");
+
+    // Add a text element for the value
+    svg.append("text")
+      .attr("id", "valueText")
+      .attr("x", scaleX(lastDataPoint.date))
+      .attr("y", scaleY(lastDataPoint.value) - 10) // position the text slightly above the circle
+      .text(`${lastDataPoint.value.toLocaleString('en-US')}`)
+      .attr("font-size", "1.7vh")
+      .attr("text-anchor", "left");
+
+    // Add a circle at the end of the total line
+    svg.append("circle")
+      .attr("id", "capacityCircle")
+      .attr("cx", scaleX(lastDataPoint.date))
+      .attr("cy", scaleY(lastDataPoint.capacity))
+      .attr("r", 5) // radius
+      .attr("fill", "white")
+      .attr("stroke", "blue")
+      .attr("stroke-width", 1.5);
+
+    // Get most recent capacity
+    let capacity = lastDataPoint.capacity;
+    // Calculate the difference
+    let difference = lastDataPoint.value - lastDataPoint.capacity;
+
+    // Select all elements with class "capacity" and update their text
+    let capacity_els = document.getElementsByClassName("capacity");
+    for (let i = 0; i < capacity_els.length; i++) {
+      capacity_els[i].textContent = capacity.toLocaleString('en-US');
+    }
+
+    // Select all elements with class "difference" and update their text
+    let difference_els = document.getElementsByClassName("difference");
+    for (let i = 0; i < difference_els.length; i++) {
+      difference_els[i].textContent = difference.toLocaleString('en-US');
+    }
+
+
+    let lastUpdate = new Date(lastDataPoint.date);
+    let options = { year: 'numeric', month: 'long', day: 'numeric' };
+    document.getElementById("lastUpdate").textContent = lastUpdate.toLocaleDateString('en-US', options);
+
+    // Call updateSize initially to set up the chart
+    updateSize();
+  }
+);
+
+// Define updateSize function
+let updateSize = () => {
+  width = chartContainer.node().clientWidth - margin.right - margin.left;
+  chartContainer.select('svg').attr('width', width + margin.right + margin.left);
+  scaleX.range([0, width]);
+
+  // Update the position of the circle
+  svg.select("#valueCircle")
+    .attr("cx", scaleX(lastDataPoint.date))
+    .attr("cy", scaleY(lastDataPoint.value));
+
+  // Update the position of the circle
+  svg.select("#capacityCircle")
+    .attr("cx", scaleX(lastDataPoint.date))
+    .attr("cy", scaleY(lastDataPoint.capacity));
+
+  // Update the position and content of the year text
+  svg.select("#yearText")
+    .attr("x", scaleX(lastDataPoint.date))
+    .attr("y", scaleY(lastDataPoint.value) - 25)
+    .text(`${lastDataPoint.date.getFullYear()}`);
+
+  // Update the position and content of the value text
+  svg.select("#valueText")
+    .attr("x", scaleX(lastDataPoint.date))
+    .attr("y", scaleY(lastDataPoint.value) - 10)
+    .text(`${lastDataPoint.value}`);
+
+  // Update x-axis
+  svg.select(".x-axis")
+    .call(d3.axisBottom(scaleX));
+
+  // Update the x-axis scale
+  xAxis.scale(scaleX);
+
+  // Redraw the x-axis with the new scale
+  xAxisGroup.call(xAxis);
+
+  // Update the y-axis tickSize
+  yAxis.tickSize(-width);
+
+  // Redraw the y-axis
+  yAxisGroup.call(yAxis);
+
+  // Update the line
+  svg.select(".line-path") // Select the path by its class
+    .datum(data) // Add this line
+    .attr("d", line); // Update the 'd' attribute with the line generator function
+
+  // Update the line
+  svg.select(".line-path2") // Select the path by its class
+    .datum(data) // Add this line
+    .attr("d", line2); // Update the 'd' attribute with the line generator function
+
+
+};
+
+// Add resize event listener to window
+window.addEventListener('resize', updateSize);
